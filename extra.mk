9ccd849fd490975611d214a37a131395e9d38008
Nanhumly <admin@warbler.cc>
2024-03-03 16:11:21 +0000

    extra: New gapps

--- extra.mk
+++ extra.mk
@@ -5,11 +5,7 @@ PRODUCT_PACKAGES += \
 # GAPPS
 ifeq ($(Lineage_GAPPS),true)
 TARGET_UNOFFICIAL_BUILD_ID := GAPPS
-$(call inherit-product, vendor/gapps/common/common-vendor.mk)
-
-# Call recording for Google Dialer
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+$(call inherit-product, vendor/gms/gms_mini.mk)
 
 PRODUCT_PACKAGES += \
     UpdaterGMSOverlay
